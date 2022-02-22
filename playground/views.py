fb9f989f6aed82c9bcfbdcdc178ce7ed0b2d14ba
Mohammad Vahabian <mohamad.vahabian@gmail.com>
2022-02-22 14:32:19 +0330

    aggregation

--- playground/views.py
+++ playground/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q ,F
+from django.db.models.aggregates import Count, Sum, Avg, Min, Max
 from store.models import Customer, Product,Order,OrderItem,Promotion
 
 
-def say_hello(request):
+def database_relational(request):
     products=Product.objects.select_related('collection').all()[:5]
     product_count=products.count()
 
@@ -21,10 +22,18 @@ def say_hello(request):
     customer_count=customers.count()
 
 
-    return render(request, 'hello.html', { 'name': 'mohamad',
+    return render(request, 'database_relational.html', { 'name': 'mohamad',
                                             'products': list(products),
                                             'product_count': product_count,
                                             'customers':list(customers),
                                             'customer_count':customer_count,
                                             'orders':list(orders),
                                             'order_count':order_count,})
+
+
+
+def Aggregating(request):
+
+    pruduct_count=Product.objects.aggregate(count=Count('id'))
+
+    return render(request, 'Aggregating.html', {'pruduct_count':pruduct_count})
