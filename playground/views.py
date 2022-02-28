284b20c8a36688fd75ae27fab0dcb1ec03a18523
Mohammad Vahabian <mohamad.vahabian@gmail.com>
2022-02-28 11:04:35 +0330

    Expression_Wrappers2

--- playground/views.py
+++ playground/views.py
@@ -15,7 +15,8 @@ def database_relational(request):
     product_count=products.count()
 
     # orders = OrderItem.objects.prefetch_related('order__customer').select_related('order').order_by('order__placed_at')[0:5]
-    orders= Order.objects.prefetch_related('orderitem_set__product').select_related('customer').order_by('placed_at')[0:30]
+    orders= Order.objects.prefetch_related(
+                                            'orderitem_set__product').select_related('customer').order_by('placed_at')[0:30]
     order_count=orders.count()
 
     # for item22 in orders:
